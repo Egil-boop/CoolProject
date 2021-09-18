768b80f923ca6f188843cf1830c6b78fb6574fcf
anders <37899614+javaelev90@users.noreply.github.com>
2021-09-18 12:10:23 +0200

    Created a cool road map for our future development

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -7,5 +7,28 @@ int main() {
 	cout << "Hello";
 	return 0; 
 
+	//Login login = new Login();
+
+	//login.login(username, password);
+	//LoginMessage msg = login.createUser();
+	//
+	//LoginMessage
+	//	errorcode
+	//	success?
+
+	//ConsertHandler 
+
+	//PaymentHandler
+	//	
+
+
+	//while (1) {
+	//	case 1
+	//		login
+	//	case 2
+	//		show conserts
+	//	case 3
+	//		exit
+	//}
 
 }
