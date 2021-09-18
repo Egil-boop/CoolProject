3753649686c0518a4b37b83ed9de49b6ff495130
Egil-boop <egge142@gmail.com>
2021-09-18 14:10:00 +0200

    User class skapad Login class skapad.

--- Project1/Project1/Login.h
+++ Project1/Project1/Login.h
@@ -1,6 +1,41 @@
 #pragma once
+
+
+using namespace std;
 class Login
 {
+
+	
+	// skapa en userklass.
+
+	//Function getUserNameAndPassword(String name, String password);
+	 // User user = new User (name, password);
+		
+	//Function createUser(String name, String password)
+	// Vill kolla så att någon med samma användarnamn inte finns. 
+	// Vill kolla så att namnet är inom ramen för hur långa namn får vara.
+	// (Kolla så att lösenordet uppfyller kraven för ett säkert lösenord.)
+	
+	// :: Sätt att skapa filer :: //
+	// #include <fstream>
+	//ofstream MyFile;
+	//MyFile.open("here.txt");
+	//MyFile << "here is some text";
+	//MyFile.close();
+
+
+	//Ett sätt att läsa och skriva från/till fil.
+
+
+	// User name, password. Består av String.
+
+	// Extra en krypterings algoritm.(Enkel) 
+
+
+
+	// Ett sätt att kolla om user finns i listan eller inte
+	// Om inte ett sätt att lägga till personen i listan av användare.
+
 
 };
 
---
+++ Project1/Project1/User.h
@@ -0,0 +1,14 @@
+#pragma once
+class User
+{
+	// :: Attributer :: //
+	// 
+	// Användarnamn
+	// Lösenord
+	// Pengar på kontot.
+	// Bokade event[]
+	// 
+	
+
+};
+
